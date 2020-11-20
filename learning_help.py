b93a18dd993acc8637e6a759228c3727b9bb5a65
Kazakov Mihail <advancerman@gmail.com>
2020-11-21 01:35:04 +0300

    Finished getting data from user in learning_help.py

--- learning_help.py
+++ learning_help.py
@@ -1,38 +1,124 @@
 from telegram import (
-    Update, CallbackQuery, User
+    Update, CallbackQuery
 )
-from telegram.ext import CallbackQueryHandler
+from telegram.ext import CallbackQueryHandler, MessageHandler, Filters
 
 from translations import gettext as _
 import translations as tr
 from keyboard_utils import make_inline_keyboard, get_main_keyboard
 
-from constants import State, CallbackQueryAnswer as Answer
+from states import State
+
+SUBJECT = 'subject'
+HELP_TYPE = 'help type'
+AWARD = 'award'
+DESCRIPTION = 'description'
+
+
+class Answer:
+    POP_SUBJ_OTHER = 'HELP_POP_SUBJ_OTHER'
+    TYPE_TOGETHER = 'TYPE_TOGETHER'
+    IM_WEAK = 'IM_WEAK'
+    IM_STRONK = 'IM_STRONK'
 
 
 def start(update: Update, context):
     reply_keyboard = make_inline_keyboard(
-        [[[_(tr.POPULAR_SUBJECT1, context), Answer.HELP_POP_SUBJ1],
-          [_(tr.POPULAR_SUBJECT2, context), Answer.HELP_POP_SUBJ2]],
-         [[_(tr.POPULAR_SUBJECT3, context), Answer.HELP_POP_SUBJ3],
-          [_(tr.POPULAR_SUBJECT_OTHER, context), Answer.HELP_POP_SUBJ_OTHER]]]
+        [[[_(tr.POPULAR_SUBJECT1, context)] * 2,
+          [_(tr.POPULAR_SUBJECT2, context)] * 2],
+         [[_(tr.POPULAR_SUBJECT3, context)] * 2,
+          [_(tr.POPULAR_SUBJECT_OTHER, context), Answer.POP_SUBJ_OTHER]]]
     )
     update.message.reply_text(_(tr.CHOOSE_YOUR_SUBJECT, context),
                               reply_markup=reply_keyboard)
-    return State.HELP_INLINE_CHOOSING
+    return State.HELP_CHOSE_SUBJECT
+
+
+def choose_type_prompt(reply, context):
+    reply_keyboard = make_inline_keyboard(
+        [[[_(tr.HELP_TYPE_TOGETHER, context), Answer.TYPE_TOGETHER]],
+         [[_(tr.HELP_TYPE_IM_WEAK, context), Answer.IM_WEAK],
+          [_(tr.HELP_TYPE_IM_STRONK, context), Answer.IM_STRONK]]]
+    )
+    reply(
+        text=_(tr.CHOOSE_HELP_TYPE, context),
+        reply_markup=reply_keyboard
+    )
+
+
+def choose_award_prompt(reply, context):
+    reply(_(tr.HELP_AWARD_PROMPT, context))
+
+
+def choose_description_prompt(reply, context):
+    reply(_(tr.HELP_DESCRIPTION_PROMPT, context))
 
 
 def chose_subject(update: Update, context):
     query: CallbackQuery = update.callback_query
     data = query.data
     query.answer()
-    query.edit_message_text(text=f"you pressed {data}")
-    update.effective_user.send_message(text="Спасибо!",
+    if data == Answer.POP_SUBJ_OTHER:
+        query.edit_message_text(_(tr.POPULAR_SUBJECT_OTHER_PROMPT, context))
+        return State.HELP_CHOSE_SUBJECT_OTHER
+
+    context.user_data[SUBJECT] = data
+    choose_type_prompt(query.edit_message_text, context)
+    return State.HELP_CHOSE_TYPE
+
+
+def chose_other_subject(update, context):
+    context.user_data[SUBJECT] = update.message.text
+    choose_type_prompt(update.effective_user.send_message, context)
+    return State.HELP_CHOSE_TYPE
+
+
+def chose_type(update, context):
+    query: CallbackQuery = update.callback_query
+    data = query.data
+    query.answer()
+    context.user_data[HELP_TYPE] = data
+
+    if data == Answer.TYPE_TOGETHER or data == Answer.IM_STRONK:
+        choose_description_prompt(query.edit_message_text, context)
+        return State.HELP_MATCHING
+    elif data == Answer.IM_WEAK:
+        choose_award_prompt(query.edit_message_text, context)
+        return State.HELP_CHOSE_AWARD
+
+    raise ValueError('Bad value from inline answer')
+
+
+def chose_award(update, context):
+    context.user_data[AWARD] = update.message.text
+    choose_description_prompt(update.effective_user.send_message, context)
+    return State.HELP_MATCHING
+
+
+def save_description_and_match(update, context):
+    context.user_data[DESCRIPTION] = update.message.text
+
+    update.effective_user.send_message("Sorry, this feature"
+                                       " is under construction",
+                                       reply_markup=get_main_keyboard(context))
+    return on_exit(update, context)
+
+
+def on_exit(update, context):
+    update.effective_user.send_message(_(tr.HELP_LAST_PHRASE, context),
                                        reply_markup=get_main_keyboard(context))
     return State.FIRST_NODE
 
 
 def get_states():
     return {
-        State.HELP_INLINE_CHOOSING: [CallbackQueryHandler(chose_subject)],
+        State.HELP_CHOSE_SUBJECT: [CallbackQueryHandler(chose_subject)],
+        State.HELP_CHOSE_SUBJECT_OTHER: [
+            MessageHandler(Filters.text, chose_other_subject)
+        ],
+        State.HELP_CHOSE_TYPE: [CallbackQueryHandler(chose_type)],
+        State.HELP_CHOSE_AWARD: [MessageHandler(Filters.text, chose_award)],
+        State.HELP_MATCHING: [
+            MessageHandler(Filters.text, save_description_and_match)
+        ],
     }
--- translations.py
+++ translations.py
@@ -1,10 +1,20 @@
 HELLO = {'ru': 'Привет', 'newru': "ПрИвЕт!"}
+
 HELP_WITH_LEARNING = {'ru': 'Нужна помощь с предметом?'}
 CHOOSE_YOUR_SUBJECT = {'ru': 'Выберите предмет'}
 POPULAR_SUBJECT1 = {'ru': 'Креатех'}
 POPULAR_SUBJECT2 = {'ru': 'КИК'}
 POPULAR_SUBJECT3 = {'ru': 'Физра'}
 POPULAR_SUBJECT_OTHER = {'ru': 'Сам введу'}
+POPULAR_SUBJECT_OTHER_PROMPT = {'ru': 'Ну вводи'}
+CHOOSE_HELP_TYPE = {'ru': 'Хто я'}
+HELP_TYPE_TOGETHER = {'ru': 'Вместе'}
+HELP_TYPE_IM_WEAK = {'ru': 'Он(а) мне'}
+HELP_TYPE_IM_STRONK = {'ru': 'Я е(й)му'}
+HELP_AWARD_PROMPT = {'ru': 'Скажи сколько готов отдать'}
+HELP_DESCRIPTION_PROMPT = {'ru': 'Расскажи о себе, что тебе нужно'}
+
+HELP_LAST_PHRASE = {'ru': 'Пока :('}
 
 
 def gettext(phrase, context):
